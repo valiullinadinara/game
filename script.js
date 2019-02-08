18b0ce6c5ee74fcb07545cb9044d7883c5eb8193
valiullinadinara <dianadura>
2019-02-08 22:29:50 +0300

    ааа
    аааа

---
+++ script.js
@@ -0,0 +1,71 @@
+$( "#clickme" ).click(function() {
+  $( "#img" ).animate({
+    opacity: 0.25,
+    left: "+=150",
+    height: "toggle"
+  }, 6000, function() {
+    // Animation complete.
+  });
+});
+
+
+
+$( "#clickme" ).click(function() {
+  $( "#img2" ).animate({
+    opacity: 0.25,
+    left: "+=-170",
+    height: "toggle"
+  }, 6000, function() {
+    // Animation complete.
+  });
+});
+
+$( "#clickme" ).click(function() {
+  $( "#img3" ).animate({
+    opacity: 0.25,
+    left: "+=130",
+    height: "toggle"
+  }, 6000, function() {
+    // Animation complete.
+  });
+});
+
+$( "#clickme" ).click(function() {
+  $( "#img4" ).animate({
+    opacity: 0.25,
+    left: "+=-140",
+    height: "toggle"
+  }, 6000, function() {
+    // Animation complete.
+  });
+});
+
+$( "#clickme" ).click(function() {
+  $( "#img5" ).animate({
+    opacity: 0.25,
+    left: "+=-120",
+    height: "toggle"
+  }, 6000, function() {
+    // Animation complete.
+  });
+});
+
+$( "#clickme" ).click(function() {
+  $( "#img6" ).animate({
+    opacity: 0.25,
+    left: "+=150",
+    height: "toggle"
+  }, 6000, function() {
+    // Animation complete.
+  });
+});
+
+$( "#clickme" ).click(function() {
+  $( "#img7" ).animate({
+    opacity: 0.25,
+    left: "+=-150",
+    height: "toggle"
+  }, 6000, function() {
+    // Animation complete.
+  });
+});
